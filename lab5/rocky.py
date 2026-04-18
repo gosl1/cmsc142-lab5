2a503d9d87a495d4bcea9602eee72f5288489a0d
gosl <160300269+gosl1@users.noreply.github.com>
2026-04-18 16:53:46 +0000

    new

--- lab5/rocky.py
+++ lab5/rocky.py
@@ -1,6 +1,8 @@
 def count(X, Z):
-    #Make changes here, remove pass
-    pass
+    m = len(X)  # rows
+    n = len(Z)  # columns
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
 
 
 if __name__ == '__main__':
@@ -9,5 +11,3 @@ if __name__ == '__main__':
         x, z = input().split()
         output = count(x, z)
         print(output)
-
-        test
